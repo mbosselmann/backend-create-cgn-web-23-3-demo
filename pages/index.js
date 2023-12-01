b7aff5747d1baee64b6f9a218b5859e3b0e4ca99
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-12-01 14:50:26 +0100

    Implement functionality to add a joke

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import JokeForm from "@/components/JokeForm/index.js";
 import JokeList from "../components/JokeList";
 
 export default function HomePage() {
-  return <JokeList />;
+  return (
+    <>
+      <JokeForm />
+      <JokeList />
+    </>
+  );
 }
